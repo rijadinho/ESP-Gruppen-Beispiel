4b83f8a417a4dbeebdb42433d60cff3bc149b46d
Ryad <rijadinho@gmail.com>
2016-12-09 19:34:26 +0100

    Interactive prompt added.

--- assa.c
+++ assa.c
@@ -12,7 +12,49 @@
 //-----------------------------------------------------------------------------
 //
 #include<stdio.h>
+#include<string.h>
 
-int main(){
+struct _Bintree_
+{
+  char data_[10];
+  struct _Bintree_ *left_;
+  struct _Bintree_ *right_;  
+};
 
+//// declares a global array of 50 characters.
+static char input[100];
+
+int main()
+{
+   
+  printf("Press Ctrl+c to Exit!\n");
+  while(1)
+  {
+    //Input (stdin) and output (stdout) declared in <stdio.h> library
+    fputs("esp>", stdout);
+    fgets(input,100,stdin);
+
+    if( strcmp( input, "\n" ) != 0 )
+      break;
+  }
+ 
+  /*FILE *fp;
+  int c;
+
+  fp = fopen ("black_fam-tree.dot", "r");
+  if (fp == NULL)
+  {
+    return -1;
+  }
+
+  printf("Reading from file:\n");
+  while((c = fgetc (fp)) != EOF)
+  {
+    printf("%c",c);
+  }
+  printf("File parsing successful...\n");
+  fclose(fp);*/
+
+
+  return 0;
 }
